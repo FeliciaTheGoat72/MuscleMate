8710a4836ec0318c76ea0094b645d2c74e0e58eb
cciri <ccirino103199@gmail.com>
2023-03-10 11:13:40 -0500

    added folders and some classes

---
+++ app/src/main/java/com/example/musclemate/Exercise.java
@@ -0,0 +1,11 @@
+package com.example.musclemate;
+
+public class Exercise {
+    private String exerciseName;
+    private int exerciseID;
+
+    public Exercise(String exerciseName, int exerciseID){
+        this.exerciseName = exerciseName;
+        this.exerciseID = exerciseID;
+    }
+}
