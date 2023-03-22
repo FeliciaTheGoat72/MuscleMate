069eb6118c8dfad00d218d002a4fc23c6fa870b6
cciri <ccirino103199@gmail.com>
2023-03-22 14:33:05 -0400

    edited exercise class

--- app/src/main/java/com/example/musclemate/Exercise.java
+++ app/src/main/java/com/example/musclemate/Exercise.java
@@ -1,11 +1,19 @@
 package com.example.musclemate;
 
 public class Exercise {
-    private String exerciseName;
-    private int exerciseID;
+    private final String exerciseName;
+    private final int exerciseID;
 
     public Exercise(String exerciseName, int exerciseID){
         this.exerciseName = exerciseName;
         this.exerciseID = exerciseID;
+    }
+
+    public String getExerciseName(){
+        return exerciseName;
+    }
+
+    public int getExerciseID(){
+        return exerciseID;
     }
 }
