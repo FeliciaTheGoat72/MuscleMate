f34a2eebf6b82d2c769c39806810c401ab0cb567
jmachcse <mach.31@buckeyemail.osu.edu>
2023-03-22 14:53:05 -0400

    removed overrides

--- app/src/main/java/com/example/musclemate/ExerciseActivities/MuscleGroupActivity.java
+++ app/src/main/java/com/example/musclemate/ExerciseActivities/MuscleGroupActivity.java
@@ -7,22 +7,22 @@ public class MuscleGroupActivity implements IExercise {
     private String muscleGroup = "Arms";
     private int exerciseID = 0;
 
-    @Override
+
     public void beginExercise(){
 
     }
 
-    @Override
+
     public void endExercise(){
 
     }
 
-    @Override
+
     public String getExerciseName(){
         return exerciseName;
     }
 
-    @Override
+
     public int getExerciseID(){
         return exerciseID;
     }
