2d7bcd4e1d931292505a7ab10c5a77568194c32c
cciri <ccirino103199@gmail.com>
2023-03-31 12:41:03 -0400

    small fixes

--- app/src/main/java/com/example/musclemate/ExerciseActivities/MuscleGroupActivity.java
+++ app/src/main/java/com/example/musclemate/ExerciseActivities/MuscleGroupActivity.java
@@ -14,30 +14,11 @@ import com.example.musclemate.ExerciseFragments.MuscleListFragment;
 import com.example.musclemate.R;
 import com.example.musclemate.databinding.MainmenuFragmentBinding;
 
-public class MuscleGroupActivity extends Fragment implements View.OnClickListener{
+public class MuscleGroupActivity extends Fragment{
 
     private MainmenuFragmentBinding binding;
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.button_chest){
-            NavHostFragment.findNavController(MuscleGroupActivity.this)
-                    .navigate(R.id.action_MainMenuFragment_to_ChestFragment);
-        } else if (v.getId() == R.id.button_back) {
-            NavHostFragment.findNavController(MuscleGroupActivity.this)
-                        .navigate(R.id.action_MainMenuFragment_to_BackFragment);
-        } else if (v.getId() == R.id.button_arms) {
-            NavHostFragment.findNavController(MuscleGroupActivity.this)
-                        .navigate(R.id.action_MainMenuFragment_to_ArmFragment);
-        } else if (v.getId() == R.id.button_legs) {
-            NavHostFragment.findNavController(MuscleGroupActivity.this)
-                        .navigate(R.id.action_MainMenuFragment_to_LegFragment);
-        } else if (v.getId() == R.id.button_abs) {
-            NavHostFragment.findNavController(MuscleGroupActivity.this)
-                        .navigate(R.id.action_MainMenuFragment_to_AbFragment);
-        }
 
-    }
 
     @Override
     public View onCreateView(
@@ -45,12 +26,6 @@ public class MuscleGroupActivity extends Fragment implements View.OnClickListene
             Bundle savedInstanceState
     ) {
         binding = MainmenuFragmentBinding.inflate(inflater, container, false);
-        binding.buttonChest.setOnClickListener(this);
-        binding.buttonBack.setOnClickListener(this);
-        binding.buttonArms.setOnClickListener(this);
-        binding.buttonLegs.setOnClickListener(this);
-        binding.buttonAbs.setOnClickListener(this);
-
         return binding.getRoot();
 
     }
